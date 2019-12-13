4a4e8a4c58268a65c18f10316f7838df2fd04bef
infojunkie <karim.ratib@gmail.com>
2019-12-13 12:24:07 -0800

    Update data and skip blank values

--- _scripts/csv2json.rb
+++ _scripts/csv2json.rb
@@ -10,8 +10,8 @@ require 'json'
 lines = CSV.open(ARGV[0], 'r:bom|utf-8').readlines
 keys = lines.delete lines.first
 File.open(ARGV[1], 'w') do |f|
- data = lines.map do |values|
-   Hash[keys.zip(values)]
- end
- f.puts JSON.pretty_generate(data)
+  data = lines.select { |values| values.any? }.map do |values|
+    Hash[keys.zip(values)]
+  end
+  f.puts JSON.pretty_generate(data)
 end
